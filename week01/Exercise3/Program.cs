2b05856f585f9775725fef87dbc53e15ad187633
Olalekan Damola <olalekandamolaisaac@gmail.com>
2026-03-08 22:28:51 -0400

    new commit

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,30 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-         Console.WriteLine("Guess My Number Game!");
-
-        // Create random number generator
-        Random randomGenerator = new Random();
-        int magicNumber = randomGenerator.Next(1, 101);
-
-        int guess = 0;
+         Random randomGenerator = new Random();
+         int magicNumber = randomGenerator.Next(1, 101);
+         
+         int guess = -1;
 
         // Loop until the user guesses correctly
-        while (guess != magicNumber)
+         while (guess != magicNumber)
         {
             Console.Write("What is your guess? ");
             guess = int.Parse(Console.ReadLine());
 
-            if (guess < magicNumber)
+            if (magicNumber > guess)
             {
                 Console.WriteLine("Higher");
             }
-            else if (guess > magicNumber)
+            else if (magicNumber < guess)
             {
                 Console.WriteLine("Lower");
             }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
         }
-
-        Console.WriteLine("You guessed it!");
     }
 }
